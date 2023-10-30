3503dfc23f88e56c180c3d9d9c004e302b705e74
tk27182 <tkirsh2@illinois.edu>
2023-10-30 13:17:55 -0500

    save results to one file

--- Code/run_models.py
+++ Code/run_models.py
@@ -30,6 +30,18 @@ import visualization as viz
 ### Star the timer
 start_time = time.perf_counter()
 
+### Make Hyperband Class update
+class HyperbandTuner(kt.Hyperband):
+    def __init__(self, hypermodel, **kwargs):
+        super().__init__(hypermodel, **kwargs)
+
+    def run_trial(self, trial, *args, **kwargs):
+        hp = trial.hyperparameters
+        model = self.hypermodel.build(hp)
+        return self.hypermodel.fit(hp, model, *args, **kwargs)
+
+
+
 ### Check GPU running
 if tf.test.gpu_device_name():
     print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
@@ -103,7 +115,7 @@ print("The type of input_bias is: ", type(input_bias))
 ### Set variables
 TIME_STEPS=24
 BATCH_SIZE=512
-overwrite=True
+overwrite=False
 
 ### Create model dictionary
 model_dict = {'lstm':                 nnm.HyperLSTM(loss=loss, num_features=num_features, binary=binary),
@@ -132,6 +144,11 @@ else:
     else:
         early_stopping = tf.keras.callbacks.History()
 
+best_model_checkpoint = tf.keras.callbacks.ModelCheckpoint(f'../Results/{directory}/{project_name}/best_model.hdf5',
+                                                            save_best_only=True,
+                                                            monitor='val_loss',
+                                                            mode='min'
+                                                            )
 ### Define the model to use
 mdl = model_dict[model_name]
 print("MODEL NAME: ", model_name)
@@ -339,9 +356,6 @@ if model_type == 'indv':
         y_test  = target[test_idx]
         y_val   = target[val_idx]
 
-    #elif (smote == 'gauss') or (smote == 'smote'):
-    #
-    #    train_data, test_data, val_data, y_train, y_test, y_val = dp.train_test_val_split(data, target, test_size=0.2, val_size=0.25)
 
     elif smote == 'downsample':
 
@@ -405,6 +419,40 @@ elif model_type == 'general':
         val_data  = data[val_idx, :]
         y_val     = target[val_idx]
 
+    elif smote == 'smote':
+
+        y_train    = target[train_idx]#.reshape((-1,1))
+        y_val      = target[val_idx]#.reshape((-1,1))
+        y_test     = target[test_idx]#.reshape((-1,1))
+
+        if len(data.shape) > 2:
+
+            train_data = []
+            val_data = []
+            new_y_train = []
+            new_y_val = []
+            for f in range(data.shape[2]):
+                tfeature_data, ty_train = dp.augment_pos_labels(data[train_idx, :, f], y_train)
+                vfeature_data, vy_val   = dp.augment_pos_labels(data[val_idx, :, f], y_val)
+
+                train_data.append(tfeature_data)
+                val_data.append(vfeature_data)
+
+                new_y_train.append(ty_train)
+                new_y_val.append(vy_val)
+
+            train_data = np.stack(train_data, axis = 2)
+            val_data   = np.stack(val_data, axis = 2)
+
+            y_train = ty_train #np.hstack(new_y_train) #np.stack(new_y_train, axis = 0)
+            y_val   = vy_val #np.hstack(new_y_val) #np.stack(new_y_val, axis = 0)
+
+        else:
+            train_data, y_train = dp.augment_pos_labels(data[train_idx], y_train)
+            val_data, y_val     = dp.augment_pos_labels(data[val_idx], y_val)
+
+        test_data  = data[test_idx]
+
     else:
         raise ValueError(f"SMOTE parameter is incorrect. Change this: {smote}")
 
@@ -428,7 +476,7 @@ print("Val: ", np.sum(y_val == 1))
 print("Test: ", np.sum(y_test == 1))
 
 # Compute the class weights
-if 'autoencoder' not in model_name:
+if ('autoencoder' not in model_name) and (smote == 'None'):
     train_weights = class_weight.compute_class_weight(class_weight='balanced',
                                     classes=np.unique(y_train.ravel()), y=y_train.ravel())
     # Reformat for tensorflow
@@ -452,8 +500,8 @@ if best_model_path.exists() and not overwrite:
         best_hps = json.load(bin)
 
 else:
-
-    tuner = kt.Hyperband(mdl,
+    # kt.Hyperband HyperbandTuner
+    tuner = HyperbandTuner(mdl,
                         objective=kt.Objective(name='val_loss', direction='min'),
                         max_epochs=MAX_EPOCHS,
                         factor=10,
@@ -464,14 +512,14 @@ else:
 
     # Run the hypertuning search
     tuner.search(train_data, y_train, epochs=MAX_EPOCHS, validation_data = (val_data, y_val), batch_size=BATCH_SIZE,
-                callbacks=[early_stopping]) #, class_weight=train_weights)
+                callbacks=[early_stopping, best_model_checkpoint], class_weight=train_weights)
 
     # Get the optimal hyperparameters
     best_hps=tuner.get_best_hyperparameters(num_trials=1)[0]
     print("The best hyperparameters are: ", best_hps.values)
     # Save the best hyperparameters
-    with open(f"../Results/{directory}/{project_name}/best_hps_{model_name}_hypertune.json", 'w') as bout:
-        json.dump(best_hps.values, bout)
+    # with open(f"../Results/{directory}/{project_name}/best_hps_{model_name}_hypertune.json", 'w') as bout:
+    #     json.dump(best_hps.values, bout)
     # Save the best model
     #tuner.get_best_models()[0].save(f"../Results/{directory}/best_model.h5")
 
@@ -479,7 +527,7 @@ else:
     model = tuner.hypermodel.build(best_hps)
 
     history = model.fit(train_data, y_train, epochs=MAX_EPOCHS, batch_size=BATCH_SIZE,
-                        validation_data=(val_data, y_val)) #, class_weight=train_weights)
+                        validation_data=(val_data, y_val), class_weight=train_weights)
     print("BEST MODEL SUMMARY: ")
     print(model.summary())
 
@@ -492,19 +540,19 @@ else:
     print(tuner.results_summary())
     # Re-instantiate hypermodel and train with optimal number of epochs
     hypermodel = tuner.hypermodel.build(best_hps)
-    ### Stop hypertuning and save
-    #hypermodel.save(best_model_path)
+
 
     # Retrain the model using the best epoch
     history = hypermodel.fit(train_data, y_train, epochs=MAX_EPOCHS, batch_size=BATCH_SIZE,
-                        validation_data=(val_data, y_val)) #, class_weight=train_weights)
-
-    viz.plot_loss(history.history['loss'], history.history['val_loss'], 'Test LSTM Loss Results', "test_loss_lstm_loss.png")
-    viz.plot_loss(history.history['recall'], history.history['val_recall'], 'Test LSTM Recall Results', "test_loss_lstm_recall.png")
-
+                        validation_data=(val_data, y_val), class_weight=train_weights)
 
     ### Stop hypertuning and save
-    # hypermodel.save(best_model_path)
+    hypermodel.save(best_model_path)
+
+    loss     = history.history['loss']
+    val_loss = history.history['val_loss']
+    # viz.plot_loss(history.history['loss'], history.history['val_loss'], 'Test LSTM Loss Results', "test_loss_lstm_loss.png")
+    # viz.plot_loss(history.history['recall'], history.history['val_recall'], 'Test LSTM Recall Results', "test_loss_lstm_recall.png")
 
     ### Remove all trials ###
     trial_path = Path(f"{directory}/{project_name}")
@@ -537,18 +585,18 @@ else:
 
 
 # Plot the AU-ROC and AU-PRC
-train_fpr, train_tpr, train_thresh = roc_curve(y_train, y_pred_train, pos_label=1)
-val_fpr, val_tpr, val_thresh       = roc_curve(y_val, y_pred_val, pos_label=1)
-test_fpr, test_tpr, test_thresh    = roc_curve(y_test, y_pred_test, pos_label=1)
+# train_fpr, train_tpr, train_thresh = roc_curve(y_train, y_pred_train, pos_label=1)
+# val_fpr, val_tpr, val_thresh       = roc_curve(y_val, y_pred_val, pos_label=1)
+# test_fpr, test_tpr, test_thresh    = roc_curve(y_test, y_pred_test, pos_label=1)
 
-train_ppr, train_rec, train_pthresh = precision_recall_curve(y_train, y_pred_train, pos_label=1)
-val_ppr, val_rec, val_pthresh       = precision_recall_curve(y_val, y_pred_val, pos_label=1)
-test_ppr, test_rec, test_pthresh    = precision_recall_curve(y_test, y_pred_test, pos_label=1)
+# train_ppr, train_rec, train_pthresh = precision_recall_curve(y_train, y_pred_train, pos_label=1)
+# val_ppr, val_rec, val_pthresh       = precision_recall_curve(y_val, y_pred_val, pos_label=1)
+# test_ppr, test_rec, test_pthresh    = precision_recall_curve(y_test, y_pred_test, pos_label=1)
 
 
 
-viz.plot_roc_curve(train_tpr, train_fpr, val_tpr, val_fpr, test_tpr, test_fpr, title = "Metrics for Test LSTM", save_name="test_loss_lstm_metrics.png")
-viz.plot_prc_curve(train_rec, train_ppr, val_rec, val_ppr, test_rec, test_ppr, title = 'Metrics for Test LSTM', save_name ="test_prc_lstm_metrics.png")
+# viz.plot_roc_curve(train_tpr, train_fpr, val_tpr, val_fpr, test_tpr, test_fpr, title = "Metrics for Test LSTM", save_name="test_loss_lstm_metrics.png")
+# viz.plot_prc_curve(train_rec, train_ppr, val_rec, val_ppr, test_rec, test_ppr, title = 'Metrics for Test LSTM', save_name ="test_prc_lstm_metrics.png")
 
 # Save the predictions
 filename = Path(f"../Results/{directory}/{'_'.join(data_name)}/{model_name}_results/")
@@ -556,34 +604,60 @@ filename = Path(f"../Results/{directory}/{'_'.join(data_name)}/{model_name}_resu
 # Make the directory if it doesn't exist
 filename.mkdir(parents=True, exist_ok=True)
 
-# Save the predictions
-if (filename / "predictions.npz").exists() and overwrite:
-    print("Overwriting the predictions...")
-    np.savez(filename / "predictions.npz", test_preds=y_pred_test, train_preds=y_pred_train, val_preds=y_pred_val)
-elif not (filename / "predictions.npz").exists():
-    print("Predictions do not exist. Saving predictions...")
-    np.savez(filename / "predictions.npz", test_preds=y_pred_test, train_preds=y_pred_train, val_preds=y_pred_val)
-else:
-    print("Predictions already exist and will not be overwritten.")
+# Save the loss
+# if (filename / "loss.npz").exists() and overwrite:
+#     print("Overwriting the loss values...")
+#     np.savez(filename / "loss.npz", loss=loss, val_loss=val_loss)
+# elif not (filename / "loss.npz").exists():
+#     print("Losses do not exist. Saving losses...")
+#     np.savez(filename / "loss.npz", loss=loss, val_loss=val_loss)
+# else:
+#     print("Losses already exist and will not be overwritten.")
 
-# Save the targets
-if (filename / "targets.npz").exists() and overwrite:
-    print("Overwriting the targets...")
-    np.savez(filename / "targets.npz", test_target=y_test, train_target=y_train, val_target=y_val)
-elif not (filename / "targets.npz").exists():
-    print("Targets do not exist. Saving targets...")
-    np.savez(filename / "targets.npz", test_target=y_test, train_target=y_train, val_target=y_val)
-else:
-    print("Targets already exist and will not be overwritten.")
+# # Save the predictions
+# if (filename / "predictions.npz").exists() and overwrite:
+#     print("Overwriting the predictions...")
+#     np.savez(filename / "predictions.npz", test_preds=y_pred_test, train_preds=y_pred_train, val_preds=y_pred_val)
+# elif not (filename / "predictions.npz").exists():
+#     print("Predictions do not exist. Saving predictions...")
+#     np.savez(filename / "predictions.npz", test_preds=y_pred_test, train_preds=y_pred_train, val_preds=y_pred_val)
+# else:
+#     print("Predictions already exist and will not be overwritten.")
+
+# # Save the targets
+# if (filename / "targets.npz").exists() and overwrite:
+#     print("Overwriting the targets...")
+#     np.savez(filename / "targets.npz", test_target=y_test, train_target=y_train, val_target=y_val)
+# elif not (filename / "targets.npz").exists():
+#     print("Targets do not exist. Saving targets...")
+#     np.savez(filename / "targets.npz", test_target=y_test, train_target=y_train, val_target=y_val)
+# else:
+#     print("Targets already exist and will not be overwritten.")
 
 elapsed_time = (time.perf_counter() - start_time)
 rez=resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1024.0/1024.0
 
-if (filename / "resource_metrics.npz").exists() and overwrite:
-    print("Overwriting the resources...")
-    np.savez(filename / "resource_metrics.npz", rez=rez, time=elapsed_time)
-elif not (filename / "resource_metrics.npz").exists():
-    print("Resources do not exist. Saving resources...")
-    np.savez(filename / "resource_metrics.npz", rez=rez, time=elapsed_time)
+# if (filename / "resource_metrics.npz").exists() and overwrite:
+#     print("Overwriting the resources...")
+#     np.savez(filename / "resource_metrics.npz", rez=rez, time=elapsed_time)
+# elif not (filename / "resource_metrics.npz").exists():
+#     print("Resources do not exist. Saving resources...")
+#     np.savez(filename / "resource_metrics.npz", rez=rez, time=elapsed_time)
+# else:
+#     print("Resources already exist and will not be overwritten.")
+
+### Save the results ###
+if (filename / "results.npz").exists() and overwrite:
+    print("Overwriting the results...")
+    np.savez(filename / "results.npz", loss=loss, val_loss=val_loss, \
+            test_target=y_test, train_target=y_train, val_target=y_val, \
+            test_preds=y_pred_test, train_preds=y_pred_train, val_preds=y_pred_val, \
+            rez=rez, time=elapsed_time)
+elif not (filename / "resultts.npz").exists():
+    print("Results do not exist. Saving Results...")
+    np.savez(filename / "results.npz", loss=loss, val_loss=val_loss, \
+            test_target=y_test, train_target=y_train, val_target=y_val, \
+            test_preds=y_pred_test, train_preds=y_pred_train, val_preds=y_pred_val, \
+            rez=rez, time=elapsed_time)
 else:
-    print("Resources already exist and will not be overwritten.")
+    print("Results already exist and will not be overwritten.")
